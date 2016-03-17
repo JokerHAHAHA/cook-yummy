055e83c9c106f290041b8b3b2695bdd04cbfe95c
Ludo-Boa <ludo.bolla@gmail.com>
2016-03-17 15:52:24 +0100

    import event

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,10 @@ function config($routeProvider) {
 		.when('/about', {
 			templateUrl: 'views/about.html'
 		})
+		.when('/events', {
+			templateUrl: 'views/events.html',
+			controller: 'eventsController'
+		})
 		.otherwise({
 			redirectTo: '/'
 		});
@@ -20,7 +24,9 @@ function run($rootScope, $location){
 angular.module('app', ['ngRoute'])
     .config(config)
     .controller('mainController', mainController)
+    .controller('eventsController', eventsController)
     .service('todoService', todoService)
+    .service('eventService', eventService)
     /*.factory('', )*/
     .run(run);
 
